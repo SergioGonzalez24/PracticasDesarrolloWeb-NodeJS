97712cf182979ae3c75dafaa38419350e528998b
sergiogonzalez24 <sergiomanuel2020@gmail.com>
2022-03-27 23:32:11 -0600

    Creacion de checker y de index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-console.log("Hello World!");
+import './styles.css';
+import isPalindrome from './checker';
+
+console.log("index.ts/start");
+
+function buttonSubmitClick() {
+    //Obtener referencia a inputText
+    const inputText = <HTMLInputElement>document.getElementById('inputText');
+    let bPalindrome = null;
+    if (inputText) {
+        //Invocar isPalindrome
+        bPalindrome = isPalindrome(inputText.value);
+        console.log(bPalindrome);
+    }
+
+    //Actualizar divResult
+    const divResult = <HTMLDivElement>document.getElementById('divResult');
+    if (divResult) {
+        if (bPalindrome) {
+            divResult.innerText = 'The word is palindrome!3'
+            //TODO: Implementa la asignación del atributo className con valor 'palindrome'
+            divResult.className = "palindrome";
+        } else {
+            divResult.innerText = 'The word is not palindrome, sorry.'
+            //TODO: Asigna a la propiedad innerText de divResult el valor 'The word is not palindrome, sorry.'
+            divResult.className = "notPalindrome";
+        }
+    }
+}
+
+const btn = document.querySelector('button');
+if (btn) {
+  console.log('attaching listener');
+  //TODO: invoca la función addEventListener de btn con parámetros 'click' y buttonSubmitClick
+  btn.addEventListener('click', buttonSubmitClick);
+}
+
+
